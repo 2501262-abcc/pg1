c6ead4e253f4e402403b440942a2285b33294cf5
2501262-abcc <2501262@s.asojuku.ac.jp>
2026-01-29 15:16:37 +0900

    Add files via upload

---
+++ 74/js/script.js
@@ -0,0 +1,79 @@
+function is(){
+    window.location.href = "1.html";
+}
+
+function ie(){
+    window.location.href = "2.html";
+}
+
+function nz(){
+    window.location.href = "3.html";
+}
+
+function at(){
+    window.location.href = "4.html";
+}
+
+function ss(){
+    window.location.href = "5.html";
+}
+
+function sg(){
+    window.location.href = "6.html";
+}
+
+function pt(){
+    window.location.href = "7.html";
+}
+
+function dk(){
+    window.location.href = "8.html";
+}
+
+function si(){
+    window.location.href = "9.html";
+}
+
+function fi(){
+    window.location.href = "10.html";
+}
+
+function cz(){
+    window.location.href = "11.html";
+}
+
+function jp(){
+    window.location.href = "12.html";
+}
+
+function my(){
+    window.location.href = "13.html";
+}
+
+function nl(){
+    window.location.href = "14.html";
+}
+
+function ca(){
+    window.location.href = "15.html";
+}
+
+function be(){
+    window.location.href = "16.html";
+}
+
+function hu(){
+    window.location.href = "17.html";
+}
+function au(){
+    window.location.href = "18.html";
+}
+
+function hr(){
+    window.location.href = "19.html";
+}
+
+function de(){
+    window.location.href = "20.html";
+}
+
